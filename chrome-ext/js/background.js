6a3623071e6d0af4517794a8d210519071d7d889
Joshua Richardson <joshua@richardson.tech>
2017-01-28 23:04:07 +0000

    Adds fully working chrome extension

---
+++ chrome-ext/js/background.js
@@ -0,0 +1,28 @@
+console.log("This is executing - loaded background script");
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        console.log("This is executing - background req");
+        if (request) {
+            var http = new XMLHttpRequest();
+            var url = "http://127.0.0.1:8136";
+            var params = "url=" + request["url"];
+            http.open("POST", url, true);
+
+            //Send the proper header information along with the request
+            http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+            http.onreadystatechange = function () {//Call a function when the state changes.
+                if (http.readyState == 4 && http.status == 200) {
+                    alert(http.responseText);
+                }
+            }
+            http.send(params);
+
+
+            // Returns true so that the rating ajax can be sent off asynchronously
+            return true;
+        }
+
+    }
+);
