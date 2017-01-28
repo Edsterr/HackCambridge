9dd61fb03a36e4ef5221353caa3f9a590879a455
GravitatingGnome <GravGnome@gmail.com>
2017-01-28 23:27:45 +0000

    post

--- server/productivitypal/server/views/productivity.py
+++ server/productivitypal/server/views/productivity.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
+@csrf_exempt
 def update(request):
-
     if(request.method == "POST"):
-        pass
+        print(json.loads(request.body))
+        return HttpResponse(request.POST)
