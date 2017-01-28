4c51386213a63c45399b6dc54f27d3d53a3b0ff1
KinRyuuXIV <tom.f.whitehouse@btinternet.com>
2017-01-28 17:02:58 +0000

    Models

--- server/productivitypal/server/urls.py
+++ server/productivitypal/server/urls.py
@@ -3,10 +3,5 @@ from django.contrib import admin
 from server import views
 
 urlpatterns = [
-from django.conf.urls import url, include
-from django.contrib import admin
-from server import views
-
-urlpatterns = [
     url(r'^productivity/update', views.productivity.update, "productivity_update"),
 ]
