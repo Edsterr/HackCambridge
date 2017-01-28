711a19d72252cfadc323c021ccfe99d091053086
GravitatingGnome <GravGnome@gmail.com>
2017-01-28 20:53:42 +0000

    keylogging

--- client/src/run_app.py
+++ client/src/run_app.py
@@ -5,9 +5,10 @@ async def run():
     session = create_session()
      
     while True:
-        session.update_focus()
-        await asyncio.sleep(0.5)
+        await session.update_focus()
+        await asyncio.sleep(0.25)
         print("Log: %s"%(str(session.focus_log )) )
+        print(session.kb_activity)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(run())
